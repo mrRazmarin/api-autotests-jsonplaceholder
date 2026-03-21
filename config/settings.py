5c2c99a5cd09ae4dfa1a6f787df0e7e8e4507985
Mikhail Salnikov <salnikovmishanew@gmail.com>
2026-03-21 15:53:16 +0300

    refactor

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,7 @@
-BASE_URL = "https://jsonplaceholder.typicode.com"
+import os
+
+# Оставляем jsonplaceholder для старых тестов
+BASE_URL = os.getenv("BASE_URL", "https://jsonplaceholder.typicode.com")
+
+# Специфичный URL для Petstore (используются для новых тестов)
+PETSTORE_URL = os.getenv("PETSTORE_URL", "http://localhost:8080/v2")
